9517f626c7fa248976649a0314ad56b1fee1edfc
bzdev87 <jawirjago0@gmail.com>
2025-03-15 12:29:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const themeToggle = document.getElementById("themeToggle");
-    themeToggle?.addEventListener("change", () => {
-        document.body.classList.toggle("dark-mode", themeToggle.checked);
-    });
-});
